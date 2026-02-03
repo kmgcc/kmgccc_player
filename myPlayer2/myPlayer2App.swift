041a2c9284f263bef1be226dc9eb8ee1634fc355
kmg <kmgccc@outlook.com>
2026-02-03 21:38:16 +0800

    Initial Commit

---
+++ myPlayer2/myPlayer2App.swift
@@ -0,0 +1,17 @@
+//
+//  myPlayer2App.swift
+//  myPlayer2
+//
+//  Created by kmg on 2026/2/3.
+//
+
+import SwiftUI
+
+@main
+struct myPlayer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
