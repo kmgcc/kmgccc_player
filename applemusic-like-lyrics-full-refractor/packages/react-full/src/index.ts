c776964dee8562fd9fccdf70ca043f96d4f6a7ba
kmg <kmgccc@outlook.com>
2026-02-07 21:20:30 +0800

    Apply gitignore to remove tracked files

--- applemusic-like-lyrics-full-refractor/packages/react-full/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./components";
-export * from "./layout";
-export * from "./utils";
-export * from "./states/dataAtoms";
-export * from "./states/configAtoms";
-export * from "./states/callbacks";
-export * from "./states/controlsAtoms";
