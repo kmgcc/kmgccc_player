d2252b9e0dbd6c1abca732a540f98b20157d3a0b
kmg <kmgccc@outlook.com>
2026-02-07 21:20:30 +0800

    Apply gitignore to remove tracked files

--- applemusic-like-lyrics-full-refractor/packages/player/src/pages/ws/send.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from "react";
-
-export const Component: FC = () => {
-	return <div>施工中，敬请期待</div>;
-};
-
-Component.displayName = "WSProtocolSendPage";
-
-export default Component;
